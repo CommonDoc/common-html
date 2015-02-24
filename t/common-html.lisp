d99eba324ca0e38eef87fdc2494342b52d368010
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-24 20:46:46 -0200

    Fix TOC tests

--- t/common-html.lisp
+++ t/common-html.lisp
@@ -291,9 +291,9 @@
                (:title (list (make-text "Section 2")))))))
     (is
      (equal (common-html.toc:single-file-toc doc)
-            "<ol class=\"toc\"><a href=\"#section-1\">Section 1</a><ol><li><a href=\"#section-1.1\">Section 1.1</a></li></ol><a href=\"#section-2\">Section 2</a></ol>"))
+            "<ol class=\"toc\"><li><a href=\"#section-1\">Section 1</a><ol><li><a href=\"#section-1.1\">Section 1.1</a></li></ol></li><li><a href=\"#section-2\">Section 2</a></li></ol>"))
     (is
      (equal (common-html.toc:multi-file-toc doc)
-            "<ol class=\"toc\"><a href=\"section-1.html\">Section 1</a><ol><li><a href=\"section-1.1.html\">Section 1.1</a></li></ol><a href=\"section-2.html\">Section 2</a></ol>"))))
+            "<ol class=\"toc\"><li><a href=\"section-1.html\">Section 1</a><ol><li><a href=\"section-1.1.html\">Section 1.1</a></li></ol></li><li><a href=\"section-2.html\">Section 2</a></li></ol>"))))
 
 (run! 'tests)
