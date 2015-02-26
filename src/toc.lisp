7062cb449603b71975a69eb00266899f8bc8dcde
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-26 19:35:36 -0200

    Update TOC code to use optional :max-depth argument

--- src/toc.lisp
+++ src/toc.lisp
@@ -1,12 +1,14 @@
 (in-package :common-html.toc)
 
-(defun single-file-toc (doc)
+(defun single-file-toc (doc &key max-depth)
   "Generate and HTML table of contents for a single-file document."
   (common-doc.ops:fill-unique-refs doc)
-  (common-html.emitter:node-to-html-string (common-doc.ops:table-of-contents doc)))
+  (common-html.emitter:node-to-html-string
+   (common-doc.ops:table-of-contents doc :max-depth max-depth)))
 
-(defun multi-file-toc (doc)
+(defun multi-file-toc (doc &key max-depth)
   "Generate and HTML table of contents for a multi-file document."
   (common-doc.ops:fill-unique-refs doc)
   (let ((common-html.multi-emit:*multi-emit* t))
-    (common-html.emitter:node-to-html-string (common-doc.ops:table-of-contents doc))))
+    (common-html.emitter:node-to-html-string
+     (common-doc.ops:table-of-contents doc :max-depth max-depth))))
